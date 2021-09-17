3dd4b09a574d12febd4bed9465068d2c860a0f73
fatih <48352716+secedit@users.noreply.github.com>
2021-09-17 23:11:07 +0200

    Update Web_deploy.py

--- Web_deploy.py
+++ Web_deploy.py
@@ -31,7 +31,7 @@ def main():
     st.markdown("<h3></h3>", unsafe_allow_html=True)
 
     # Setting Application sidebar default
-    image = Image.open('Pycoders.jpg')
+    image = Image.open('Pycoders.jpeg')
     add_selectbox = st.sidebar.selectbox(
         "How would you like to predict?", ("Online", "Batch"))
     st.sidebar.info('This app is created to predict Customer Churn')
